2122a363259452148cfcaa0ad12c646883b2350f
Chandler <chndlr.wllm@gmail.com>
2018-01-21 21:12:19 -0600

    initiate authentication - user model

--- models/box.js
+++ models/box.js
@@ -2,12 +2,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const boxSchema = new Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-  description: { type: String, required: true },
-  quantity: { type: Number, required: false },
-  date: { type: Date, default: Date.now }
+  title: { 
+    type: String, 
+    required: true 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  image: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  quantity: { 
+    type: Number, 
+    required: false 
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 const Box = mongoose.model("Box", boxSchema);
