9b3d94e760b1b1d2c38d5ae3641803f6131dd11e
Chandler <chndlr.wllm@gmail.com>
2018-03-19 16:30:30 -0500

    updated signup form with mailchimp code

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -18,7 +18,7 @@ const Foot = () => (
       <Row>
         <div id="mailing" style={{textAlign: "center"}}>
           <Col size="md-2" />
-          <Col size="md-4">
+          {/* <Col size="md-4">
             <TextField
               hintText="supermama@hero.org"
               floatingLabelText="SIGN UP"
@@ -36,6 +36,28 @@ const Foot = () => (
               underlineStyle={{ borderColor: "rgb(90,90,90)" }}
             />
             <FlatButton label="Submit" hoverColor="rgb(0, 178, 169)" />
+          </Col> */}
+
+          <Col size="md-4">
+          
+            <div id="mc_embed_signup">
+            <form action="https://lamamacare.us17.list-manage.com/subscribe/post?u=2442aa5f68e55a714269be4e1&amp;id=7c3c6a8080" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                
+                <div id="mc_embed_signup_scroll">
+                
+                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required/>
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_2442aa5f68e55a714269be4e1_7c3c6a8080" tabindex="-1" value=""/>
+                  </div>
+                  <div class="clear">
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
+                  </div>
+                  
+                </div>
+
+              </form>
+            </div>
+
           </Col>
 
           {/* <Col size="md-2" /> */}
