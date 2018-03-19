ec3256037f92851ef820711f9cf9de9e1b51e513
Chandler <chndlr.wllm@gmail.com>
2018-03-19 17:46:51 -0500

    updated build with revised signup code

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -40,20 +40,17 @@ const Foot = () => (
 
           <Col size="md-4">
           
-            <div id="mc_embed_signup">
-            <form action="https://lamamacare.us17.list-manage.com/subscribe/post?u=2442aa5f68e55a714269be4e1&amp;id=7c3c6a8080" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                
-                <div id="mc_embed_signup_scroll">
-                
-                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required/>
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                    <input type="text" name="b_2442aa5f68e55a714269be4e1_7c3c6a8080" tabindex="-1" value=""/>
-                  </div>
-                  <div class="clear">
-                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/>
-                  </div>
+            <div id="mc_embed_signup" style={{background:"#fff", clear:"left", font:"14px, Helvetica,Arial,sans-serif", width:"100%"}}>
+              <form action="https://lamamacare.us17.list-manage.com/subscribe/post?u=2442aa5f68e55a714269be4e1&amp;id=7c3c6a8080" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                   
-                </div>
+                  <div id="mc_embed_signup_scroll">
+                  
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required/>
+                      <div style={{position: "absolute", left: "-5000px", ariaHidden: "true"}}><input type="text" name="b_2442aa5f68e55a714269be4e1_7c3c6a8080" tabindex="-1" value=""/>
+                      </div>
+                    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"/></div>
+                    
+                  </div>
 
               </form>
             </div>
